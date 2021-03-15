2111f6053a29e603eefb4ebf1f4c2a3529c646da
Reed560 <55811906+Reed560@users.noreply.github.com>
2021-03-15 17:06:51 -0400

    Update app.module.ts

--- IHSAA-app/src/app/app.module.ts
+++ IHSAA-app/src/app/app.module.ts
@@ -3,18 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 import { SchoolYearDDComponent } from './school-year-dd/school-year-dd.component';
 import { GenderDDComponent } from './gender-dd/gender-dd.component';
 import { SportDDComponent } from './sport-dd/sport-dd.component';
 import { ClassSizeDDComponent } from './class-size-dd/class-size-dd.component';
+=======
+import { DatabaseComponentComponent } from './database-component/database-component.component';
+import { AlgorithmTYPESCRIPTComponent } from './algorithm-typescript/algorithm-typescript.component';
+import { SectionalGroupComponent } from './sectional-group/sectional-group.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< Updated upstream
     SchoolYearDDComponent,
     GenderDDComponent,
     SportDDComponent,
     ClassSizeDDComponent
+=======
+    DatabaseComponentComponent,
+    AlgorithmTYPESCRIPTComponent,
+    SectionalGroupComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
