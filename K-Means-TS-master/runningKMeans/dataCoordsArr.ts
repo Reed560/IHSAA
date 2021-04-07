2aa5859a748212c6c4dbe6f73cd4125ef8213e9b
Sasha <grigortsuk2003@gmail.com>
2021-04-07 11:50:22 -0400

    Adding comments and cleaning up

--- K-Means-TS-master/runningKMeans/dataCoordsArr.ts
+++ K-Means-TS-master/runningKMeans/dataCoordsArr.ts
@@ -1,13 +1,17 @@
+/*
+This takes text file latLongCoords and puts it in array
+Order is from Google Sheets, or order from Excel file of schools.
+Eventually, instead of pulling from txt file, we would want to pull from IHSAA database
+This is array is exported to use in kMeansTesting2 and findingHosts
+*/
+
 import fs = require('fs');
 
 var text = fs.readFileSync('latLongCoords.txt','utf8');
 
 var stringData = text.split(' ');
-//console.log(stringData);
 
-//var myFloat = parseFloat(data);
 const data = stringData.map((i) => Number(i));
-//console.log(data);
 
 export const dataArray: number[][] = [];
 
@@ -15,6 +19,3 @@ for(let i = 0; i<data.length-1; i++){
 	if(i%2 == 0)
     dataArray.push( [data[i], data[i+1]] );
 }
-
-//console.log(dataArray);
-
